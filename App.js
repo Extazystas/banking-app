c24be4eab8f2aaff0aa1e2d9bddf9ecd14b6bc26
extazystas <extazystas@gmail.com>
2020-10-13 01:51:23 +0300

    Implement basic currencies data fetch
    Description:
    - fetch data from server API
    - use day.js for dates formatting
    - provide message if no data found
    - show fetched from API data

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, Text, FlatList, View } from 'react-native';
+import dayjs from 'dayjs';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [currenciesData, setCurrenciesData] = useState([]);
+  const today = dayjs().format('YYYY-MM-DD');
+
+  function fetchCountriesData() {
+    fetch(`http://127.0.0.1:3000/api/v1/currencies?day=${today}`)
+      .then((response) => response.json())
+      .then((json) => setCurrenciesData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }
+
+  useEffect(() => {
+    if (isLoading) fetchCountriesData();
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ { flex: 1, padding: 24 } }>
+      { isLoading ? <ActivityIndicator /> : (
+        currenciesData ? (
+          <FlatList
+            data={ Object.values(currenciesData.daily_rates) }
+            contentContainerStyle={ styles.container }
+            keyExtractor={ item => item.cur_abbreviation }
+            renderItem={ ({ item }) => {
+              return (
+                <View style={ { flex: 1, flexDirection: 'row', margin: 5, borderWidth: 1 } }>
+                  <Text style={styles.text}>{ item.cur_quot_name }</Text>
+                  <Text style={styles.text}>{ item.cur_abbreviation }</Text>
+                  <Text style={styles.text}>{ item.cur_official_rate }</Text>
+                </View>
+              )}
+            }
+          />
+        ) : <Text>No currencies found for selected date.</Text>
+      ) }
     </View>
   );
 }
@@ -15,7 +47,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'left',
+    justifyContent: 'left',
+  },
+  text: {
+    flex: 1,
+    flexDirection: 'column',
+    fontSize: 20,
+    margin: 10
   },
 });
