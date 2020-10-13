d6d33a5444b1227f1049e54353ef56ad2690a6a5
extazystas <extazystas@gmail.com>
2020-10-13 19:48:13 +0300

    Add currencies icons and update css

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { ActivityIndicator, StyleSheet, Text, FlatList, View } from 'react-native';
 import dayjs from 'dayjs';
+import CurrencyFlag from 'react-currency-flags';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
+  const [currenciesDay, setCurrenciesDay] = useState(dayjs().format('YYYY-MM-DD'));
   const [currenciesData, setCurrenciesData] = useState([]);
-  const today = dayjs().format('YYYY-MM-DD');
 
   function fetchCountriesData() {
-    fetch(`http://127.0.0.1:3000/api/v1/currencies?day=${today}`)
+    fetch(`http://127.0.0.1:3000/api/v1/currencies?day=${currenciesDay}`)
       .then((response) => response.json())
       .then((json) => setCurrenciesData(json))
       .catch((error) => console.error(error))
@@ -20,7 +21,10 @@ export default function App() {
   })
 
   return (
-    <View style={ { flex: 1, padding: 24 } }>
+    <View style={ { flex: 1, padding: 50, backgroundColor: '#ccc' } }>
+      <Text style={ styles.text }>
+        Сurrencies of Belarusian ruble (<b>BYN</b>) for <b>{ currenciesDay }</b>. Provided by <a href='http://nbrb.by' target='_blank'>National Bank of the Republic of Belarus</a>
+      </Text>
       { isLoading ? <ActivityIndicator /> : (
         currenciesData ? (
           <FlatList
@@ -29,10 +33,13 @@ export default function App() {
             keyExtractor={ item => item.cur_abbreviation }
             renderItem={ ({ item }) => {
               return (
-                <View style={ { flex: 1, flexDirection: 'row', margin: 5, borderWidth: 1 } }>
+                <View style={ { flex: 0.3, flexDirection: 'row', margin: 5, borderWidth: 1 } }>
                   <Text style={styles.text}>{ item.cur_quot_name }</Text>
-                  <Text style={styles.text}>{ item.cur_abbreviation }</Text>
-                  <Text style={styles.text}>{ item.cur_official_rate }</Text>
+                  <Text style={ styles.text }>
+                    <CurrencyFlag style={ { marginRight: 5}} currency={ item.cur_abbreviation } size='lg' />
+                    { item.cur_abbreviation }
+                  </Text>
+                  <Text style={ styles.text }><b>{ item.cur_official_rate }</b> BYN</Text>
                 </View>
               )}
             }
@@ -49,6 +56,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'left',
     justifyContent: 'left',
+    backgroundColor: '#ccc'
   },
   text: {
     flex: 1,
